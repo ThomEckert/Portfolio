a12273eb391f54c0a1a61f9449398960b2d720f1
ThomEckert <thec.lernt@gmx.de>
2024-06-10 17:11:15 +0200

    Skill set

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LetsWorkTogetherComponent } from './lets-work-together/lets-work-together.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, LetsWorkTogetherComponent],
+  imports: [LandingPageComponent, LetsWorkTogetherComponent, SkillSetComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
