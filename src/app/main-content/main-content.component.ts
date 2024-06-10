4f98bca1667d319192068543d88dd2d0dc8bc630
ThomEckert <thec.lernt@gmx.de>
2024-06-10 18:07:25 +0200

    interplementet footer

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LetsWorkTogetherComponent } from './lets-work-together/lets-work-together.component';
 import { SkillSetComponent } from './skill-set/skill-set.component';
 import { MyWorkComponent } from './my-work/my-work.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [LandingPageComponent, LetsWorkTogetherComponent, SkillSetComponent, MyWorkComponent],
+  imports: [LandingPageComponent, LetsWorkTogetherComponent, SkillSetComponent, MyWorkComponent, ContactComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
