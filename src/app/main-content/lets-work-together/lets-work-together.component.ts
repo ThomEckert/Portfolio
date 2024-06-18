3d194d98133ebb3d202d9a47ba7be42ef7008aba
ThomEckert <thec.lernt@gmx.de>
2024-06-18 13:42:10 +0200

    translate skill set

--- src/app/main-content/lets-work-together/lets-work-together.component.ts
+++ src/app/main-content/lets-work-together/lets-work-together.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-lets-work-together',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './lets-work-together.component.html',
   styleUrl: './lets-work-together.component.scss'
 })
